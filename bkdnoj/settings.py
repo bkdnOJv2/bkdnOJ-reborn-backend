b963b0ffc21bf574f02f30906e6b1c5224c1a38e
nvatuan <24392632+nvatuan@users.noreply.github.com>
2022-04-15 06:04:38 +0700

    Added corsheaders

--- bkdnoj/settings.py
+++ bkdnoj/settings.py
@@ -15,7 +15,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -25,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     # Local Apps
     'usergroup',
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -109,6 +110,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
 
 # REST Framework settings
 REST_FRAMEWORK = {
--- organization/models.py
+++ organization/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 class Organization(models.Model):
-	shortname = models.SlugField(
-		db_index=True,
-		unique=True,
-	)
+	shortname = models.SlugField(db_index=True, unique=True)
 	name = models.CharField(max_length=256, null=False, blank=False)
 	description = models.TextField(null=False, blank=False)
