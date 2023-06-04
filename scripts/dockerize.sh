e104cb7ab095f6624b1779a3a30c367c25134fff
nvat <24392632+nvatuan@users.noreply.github.com>
2023-06-04 21:41:12 +0700

    Update scripts and workflow for Docker (#35)
    * Update scripts and workflow
    
    * Test dockerize on push to tasks branch
    
    * Remove test code
    
    * Update CI on tasks branch

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -5,14 +5,16 @@ source ./scripts/utils/prettyecho.sh
 
 IMAGE_NAME=""
 IMAGE_DOCKERFILE_SUBDIR=""
+
 readonly IMAGE_TYPE=$1
 readonly IMAGE_TAG=$2
+readonly PUSH_TO_REMOTE=$3
 
 if [[ $IMAGE_TYPE == "app" ]]; then
-    IMAGE_NAME="bkdnoj-v2/backend"
+    IMAGE_REPOSITORY="$DOCKER_USERNAME/bkdnoj-v2_api"
     IMAGE_DOCKERFILE_SUBDIR="app"
 elif [[ $IMAGE_TYPE == "migrate" ]]; then
-    IMAGE_NAME="bkdnoj-v2/migrate"
+    IMAGE_REPOSITORY="$DOCKER_USERNAME/bkdnoj-v2_db-migrate"
     IMAGE_DOCKERFILE_SUBDIR="migrate"
 else
     echo "FATAL: unrecognize image type '$IMAGE_TYPE'"
@@ -26,4 +28,8 @@ fi
 
 docker build \
     --progress=plain \
-    -f ./docker/$IMAGE_DOCKERFILE_SUBDIR/Dockerfile -t $IMAGE_NAME ./app
+    -f ./docker/$IMAGE_DOCKERFILE_SUBDIR/Dockerfile -t ${IMAGE_REPOSITORY}:${IMAGE_TAG} ./app
+
+if [[ $PUSH_TO_REMOTE == "push" ]]; then
+    docker push ${IMAGE_REPOSITORY}:${IMAGE_TAG}
+fi
