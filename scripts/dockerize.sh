61a98339f0a02b841eb4523bbf51fb990615baa4
nvatuan <nvatuan2000@gmail.com>
2023-06-04 17:39:32 +0700

    Update image type migrate

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -3,8 +3,27 @@
 set -e
 source ./scripts/utils/prettyecho.sh
 
-IMAGE_NAME="bkdnoj-v2/backend"
+IMAGE_NAME=""
+IMAGE_DOCKERFILE_SUBDIR=""
+readonly IMAGE_TYPE=$1
+readonly IMAGE_TAG=$2
+
+if [[ $IMAGE_TYPE == "app" ]]; then
+    IMAGE_NAME="bkdnoj-v2/backend"
+    IMAGE_DOCKERFILE_SUBDIR="app"
+elif [[ $IMAGE_TYPE == "migrate" ]]; then
+    IMAGE_NAME="bkdnoj-v2/migrate"
+    IMAGE_DOCKERFILE_SUBDIR="migrate"
+else
+    echo "FATAL: unrecognize image type '$IMAGE_TYPE'"
+    exit 1
+fi
+
+if [[ $IMAGE_TAG == "" ]]; then
+    echo "FATAL: empty image tag"
+    exit 1
+fi
 
 docker build \
     --progress=plain \
-    -f ./docker/app/Dockerfile -t $IMAGE_NAME ./app 
+    -f ./docker/$IMAGE_DOCKERFILE_SUBDIR/Dockerfile -t $IMAGE_NAME ./app
