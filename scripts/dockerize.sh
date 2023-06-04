65a451ae6523cd4009c900530eeab90d7d35043c
nvatuan <nvatuan2000@gmail.com>
2023-06-04 21:04:00 +0700

    Update scripts and workflow

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -5,14 +5,16 @@ source ./scripts/utils/prettyecho.sh
 
 IMAGE_NAME=""
 IMAGE_DOCKERFILE_SUBDIR=""
+
 readonly IMAGE_TYPE=$1
 readonly IMAGE_TAG=$2
+readonly PUSH_TO_REMOTE=$3
 
 if [[ $IMAGE_TYPE == "app" ]]; then
-    IMAGE_NAME="bkdnoj-v2/backend"
+    IMAGE_REPOSITORY="$DOCKER_USER/bkdnoj-v2_api"
     IMAGE_DOCKERFILE_SUBDIR="app"
 elif [[ $IMAGE_TYPE == "migrate" ]]; then
-    IMAGE_NAME="bkdnoj-v2/migrate"
+    IMAGE_REPOSITORY="$DOCKER_USER/bkdnoj-v2_db-migrate"
     IMAGE_DOCKERFILE_SUBDIR="migrate"
 else
     echo "FATAL: unrecognize image type '$IMAGE_TYPE'"
@@ -24,6 +26,12 @@ if [[ $IMAGE_TAG == "" ]]; then
     exit 1
 fi
 
-docker build \
-    --progress=plain \
-    -f ./docker/$IMAGE_DOCKERFILE_SUBDIR/Dockerfile -t $IMAGE_NAME ./app
+echo "Building: docker build -f ./docker/$IMAGE_DOCKERFILE_SUBDIR/Dockerfile -t ${IMAGE_REPOSITORY}:${IMAGE_TAG} ./app" 
+# docker build \
+#     --progress=plain \
+#     -f ./docker/$IMAGE_DOCKERFILE_SUBDIR/Dockerfile -t ${IMAGE_REPOSITORY}:${IMAGE_TAG} ./app
+
+if [[ $PUSH_TO_REMOTE == "push" ]]; then
+    echo "Pushing to ${IMAGE_REPOSITORY}:${IMAGE_TAG}"
+    # docker push ${IMAGE_REPOSITORY}:${IMAGE_TAG}
+fi
