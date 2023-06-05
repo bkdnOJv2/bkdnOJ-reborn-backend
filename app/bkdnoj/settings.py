0c27ccc64e9f4d8bfa329b1f8ee690b34b552566
nvatuan <nvatuan2000@gmail.com>
2023-06-05 23:11:16 +0700

    Revert bkdnoj/settings.py

--- app/bkdnoj/settings.py
+++ app/bkdnoj/settings.py
@@ -97,7 +97,9 @@ WSGI_APPLICATION = 'bkdnoj.wsgi.application'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'react_app'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -153,7 +155,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'react_app', 'static'),
+]
 
 # Fixture folder
 FIXTURE_DIRS = [ os.path.join(BASE_DIR, 'bkdnoj', 'fixture'), ]
