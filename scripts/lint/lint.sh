dc2668084742c8e5aa275a0830b323f8cf6d64e9
Touya (nvatuan) <lambda.nvat@gmail.com>
2024-01-13 09:46:15 +0000

    Add lint scripts

--- scripts/lint/lint.sh
+++ scripts/lint/lint.sh
@@ -1,37 +1,7 @@
 #!/bin/bash
-# Loads necessaries
-set -e # Exit on error
 
-source ./scripts/utils/prettyecho.sh
-source ./scripts/lint/setupenv.sh
-
-VERBOSE=false
-KEEP_CONTAINER=false
-# Parse options
-while getopts "v" opt; do
-  case $opt in
-    v)
-      VERBOSE=true
-      ;;
-    d)
-      KEEP_CONTAINER=true
-      ;;
-    \?)
-      efatal "Lint" "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-  esac
-done
-
-# Tearing down
-spin_up
-if [ $? -ne 0 ]; then
-  efatal 0 "Lint" "Failed to spin up container."
-  exit 1
-fi
-
-# Code
-einfo 0 "Lint" "ERRORS ONLY MODE. Ignoring warnings and bad conventions.."
+CLNORMAL=$(tput sgr0)
+CLCYAN=$(tput setaf 6)
 
 component=$1
 if [ component = "" ]; then
@@ -39,19 +9,12 @@ if [ component = "" ]; then
 else
   component="app/$component"
 fi
-einfo 1 "Lint" "Linting $component..."
 
-docker exec -t $TMP_LINTER_NAME pylint --output-format=colorized \
-    --django-settings-module=bkdnoj.settings \
-    --load-plugins pylint_django \
-    --ignore=migrations \
-    --errors-only \
-    $component 
+echo "${CLCYAN} * [ LINT ] Linting $component...${CLNORMAL}"
 
-# Tearing down
-if [ "$KEEP_CONTAINER" = "false" ]; then
-  einfo 0 "Lint" "Tearing down temporary container for lint..."
-  tear_down
-else
-  einfo 0 "Lint" "Keeping container."
-fi
+pylint --output-format=colorized \
+  --django-settings-module=bkdnoj.settings \
+  --load-plugins pylint_django \
+  --ignore=migrations \
+  --errors-only \
+  $component 
--- scripts/lint/setupenv.sh
+++ scripts/lint/setupenv.sh
@@ -7,39 +7,37 @@ TMP_LINTER_IMAGE_NAME="bkdnoj-lint-image:local"
 CONTAINER_WORKDIR="/usr/app"
 
 spin_up() {
-  set +e
-
-  echo ""
-  einfo 1 "Lint | Setup" "Spinning up temporary container for lint..."
+  einfo 1 "SETUP" "Spinning up temporary container for lint..."
 
   # Check if container is already running
-  einfo 2 "Lint | Setup" "Checking if container is already running..."
+  einfo 2 "SETUP" "Checking if container is already running..."
 
   local msg=$(docker inspect $TMP_LINTER_NAME 2> /dev/null);
   if [ "$msg" != "[]" ]; then
-    ewarn 2 "Lint | Setup" "Container is already running. Skipping..."
+    ewarn 2 "SETUP" "Container is already running. Skipping..."
     return 0
   else
-    einfo 2 "Lint | Setup" "Container is not running."
+    einfo 2 "SETUP" "Container is not running."
   fi
 
   # Container is not up, spin up a new one
-  einfo 2 "Lint | Setup" "Building image..."
-  docker build -t $TMP_LINTER_IMAGE_NAME -f ./scripts/lint/Dockerfile .
+  einfo 2 "SETUP" "Building image..."
+  set -e
+  docker build --progress=plain -t $TMP_LINTER_IMAGE_NAME -f ./scripts/lint/Dockerfile .
+  set +e
 
-  einfo 2 "Lint | Setup" "Running container in the background..."
+  einfo 2 "SETUP" "Running container in the background..."
   docker run -t -d --name $TMP_LINTER_NAME -v ".:$CONTAINER_WORKDIR" -w $CONTAINER_WORKDIR \
     $TMP_LINTER_IMAGE_NAME bash
 
-  esucceed 2 "Lint | Setup" "Container is ready."
+  esucceed 2 "SETUP" "Container is ready."
 
   echo ""
-  set -e
 }
 
 tear_down() {
   echo ""
-  einfo 1 "Lint" "Tearing down temporary container for lint..."
+  einfo 1 "SETUP" "Tearing down temporary container for lint..."
   docker rm -f $TMP_LINTER_NAME
   echo ""
 }
